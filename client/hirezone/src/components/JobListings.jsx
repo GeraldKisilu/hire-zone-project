d6fbea0bbd28defbb9942098184718f84178be71
geraldkisilu <muligerald6@gmail.com>
2024-07-10 14:35:40 +0300

    commit

--- client/hirezone/src/components/JobListings.jsx
+++ client/hirezone/src/components/JobListings.jsx
@@ -1,55 +1,55 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import './JobListings.css'; // Import the CSS file for the JobListings component
+import './JobListings.css';
 
 const JobListings = () => {
     const [selectedCategory, setSelectedCategory] = useState('All');
     const [searchTerm, setSearchTerm] = useState('');
     const [selectedJob, setSelectedJob] = useState(null);
-    const navigate = useNavigate(); // Use the useNavigate hook to programmatically navigate
+    const navigate = useNavigate(); 
 
     const jobCategories = {
         Engineering: [
-            { title: 'Software Engineer', company: 'ABC Tech', location: 'New York, NY' },
-            { title: 'Civil Engineer', company: 'XYZ Constructions', location: 'Los Angeles, CA' },
-            { title: 'Mechanical Engineer', company: 'DEF Manufacturing', location: 'Chicago, IL' },
-            { title: 'Electrical Engineer', company: 'GHI Energy', location: 'Houston, TX' },
+            { title: 'Software Engineer', company: 'ABC Tech', location: 'Dubai, UAE' },
+            { title: 'Civil Engineer', company: 'XYZ Constructions', location: 'Bangladesh, India' },
+            { title: 'Mechanical Engineer', company: 'DEF Manufacturing', location: 'Johannesburg, SouthAfrica' },
+            { title: 'Electrical Engineer', company: 'GHI Energy', location: 'Houston, Texas' },
             { title: 'Chemical Engineer', company: 'JKL Chemicals', location: 'San Francisco, CA' },
         ],
         Commerce: [
-            { title: 'Accountant', company: 'MNO Finance', location: 'New York, NY' },
-            { title: 'Financial Analyst', company: 'PQR Investments', location: 'Boston, MA' },
+            { title: 'Accountant', company: 'MNO Finance', location: 'Paris, France' },
+            { title: 'Financial Analyst', company: 'PQR Investments', location: 'Napoli, Italy' },
             { title: 'Marketing Manager', company: 'STU Marketing', location: 'Los Angeles, CA' },
-            { title: 'Business Analyst', company: 'VWX Consulting', location: 'Chicago, IL' },
-            { title: 'Sales Manager', company: 'YZA Sales', location: 'San Francisco, CA' },
+            { title: 'Business Analyst', company: 'VWX Consulting', location: 'Al Nassr, SAudi Arabia' },
+            { title: 'Sales Manager', company: 'YZA Sales', location: 'Nairobi , Kenya' },
         ],
         Healthcare: [
-            { title: 'Registered Nurse', company: 'HealthCare Inc.', location: 'New York, NY' },
-            { title: 'Pharmacist', company: 'PharmaLife', location: 'Los Angeles, CA' },
-            { title: 'Medical Assistant', company: 'MedAssist', location: 'Chicago, IL' },
-            { title: 'Lab Technician', company: 'LabCorp', location: 'Houston, TX' },
-            { title: 'Physical Therapist', company: 'TherapyPlus', location: 'San Francisco, CA' },
+            { title: 'Registered Nurse', company: 'HealthCare Inc.', location: 'Madrid, Spain' },
+            { title: 'Pharmacist', company: 'PharmaLife', location: 'Ohio, USA' },
+            { title: 'Medical Assistant', company: 'MedAssist', location: 'Chicago, Ilanois' },
+            { title: 'Lab Technician', company: 'LabCorp', location: 'Cairo, Egypt' },
+            { title: 'Physical Therapist', company: 'TherapyPlus', location: 'Atalanta, Italy' },
         ],
         Education: [
-            { title: 'Teacher', company: 'ABC School', location: 'New York, NY' },
-            { title: 'Professor', company: 'XYZ University', location: 'Los Angeles, CA' },
+            { title: 'Teacher', company: 'ABC School', location: 'Arizona, USA' },
+            { title: 'Professor', company: 'XYZ University', location: 'Alberta, Canada' },
             { title: 'School Counselor', company: 'DEF High School', location: 'Chicago, IL' },
-            { title: 'Librarian', company: 'GHI Library', location: 'Houston, TX' },
-            { title: 'Instructional Designer', company: 'JKL Education', location: 'San Francisco, CA' },
+            { title: 'Librarian', company: 'GHI Library', location: 'Hainan, China' },
+            { title: 'Instructional Designer', company: 'JKL Education', location: 'Tasmania, Australia' },
         ],
         IT: [
-            { title: 'Web Developer', company: 'ABC Web', location: 'New York, NY' },
+            { title: 'Web Developer', company: 'ABC Web', location: 'Hokkaido, Japan' },
             { title: 'System Administrator', company: 'XYZ Systems', location: 'Los Angeles, CA' },
-            { title: 'Network Engineer', company: 'DEF Networks', location: 'Chicago, IL' },
-            { title: 'Data Scientist', company: 'GHI Data', location: 'Houston, TX' },
-            { title: 'Cybersecurity Analyst', company: 'JKL Security', location: 'San Francisco, CA' },
+            { title: 'Network Engineer', company: 'DEF Networks', location: 'Azores, Portugal' },
+            { title: 'Data Scientist', company: 'GHI Data', location: 'Brittany, France' },
+            { title: 'Cybersecurity Analyst', company: 'JKL Security', location: 'Bavaria, Germany' },
         ],
     };
 
     const applyForJob = (job) => {
         setSelectedJob(job);
         alert(`Your application for the position of ${job.title} at ${job.company} is pending, proceed to Log In to complete.`);
-        navigate('/login'); // Redirect to the login page
+        navigate('/login'); // Should redirect to the login page
     };
 
     const filteredJobs = Object.keys(jobCategories)
